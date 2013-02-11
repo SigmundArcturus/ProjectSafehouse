170b45a36b9b32e1f9c6133640b6a0cc0ba705ff
FrankieAvocado <lutemaster@gmail.com>
2013-02-11 16:06:52 -0500

    Moved javascript out to seperate files and began code standard documentation

---
+++ ProjectSafehouse/Scripts/Login/Index.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    $('div.CleanBox').on('focus', 'input', function () {
+        var parent = $(this).parent();
+        parent.addClass('SelectedWrapper');
+    }).on('blur', 'input', function () {
+        var parent = $(this).parent();
+        parent.removeClass('SelectedWrapper');
+    });
+
+    $('#signUpSwitch').click(function () {
+        $('#loginBox').animate(
+            {
+                height: 0,
+                opacity: 0
+            }, 400, function () {
+                $('#loginBox').css("display", "none");
+                $('#signupBox').css("display", "block").animate(
+                    {
+                        opacity: 1,
+                        height: 360
+                    }, 400
+                 );
+            }
+        );
+    });
+
+    $('#logInSwitch').click(function () {
+        $('#signupBox').animate(
+           {
+               height: 0,
+               opacity: 0
+           }, 400, function () {
+               $('#signupBox').css("display", "none");
+               $('#loginBox').css("display", "block").animate(
+                   {
+                       opacity: 1,
+                       height: 260
+                   }, 400
+                );
+           }
+       );
+    });
+
+});
---
+++ ProjectSafehouse/Scripts/Shared/_Layout.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('.NavDropDown').click(function () {
+        if ($('#subMenu').height() < 1) {
+            $('#subMenu').animate(
+                {
+                    height: '72px'
+                },
+                400,
+                function () { }
+            );
+        }
+        else {
+            $('#subMenu').animate(
+                {
+                    height: '0px'
+                },
+                400,
+                function () { }
+            );
+        }
+    });
+});
