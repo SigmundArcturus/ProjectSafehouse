680353fb6355034c980985571557d51ca9829b87
FrankieAvocado <lutemaster@gmail.com>
2013-02-12 15:49:19 -0500

    Implemented a crude dynamic menu system.  Refine in future releases.

--- ProjectSafehouse/Scripts/Shared/_Layout.js
+++ ProjectSafehouse/Scripts/Shared/_Layout.js
@@ -1,22 +1,5 @@
 ﻿$(document).ready(function () {
     $('.NavDropDown').click(function () {
-        if ($('#subMenu').height() < 1) {
-            $('#subMenu').animate(
-                {
-                    height: '72px'
-                },
-                400,
-                function () { }
-            );
-        }
-        else {
-            $('#subMenu').animate(
-                {
-                    height: '0px'
-                },
-                400,
-                function () { }
-            );
-        }
+        
     });
 });
---
+++ ProjectSafehouse/Scripts/safehouse.menu.js
@@ -0,0 +1,92 @@
+﻿var menuMethods = extend(ProjectSafehouse, "ProjectSafehouse.Utilities.Menu");
+
+$(document).ready(function () {
+    $('#mainMenu').on('click', '.NavDropDown', function () {
+        menuMethods.ToggleSubMenu($(this).attr('id'));
+    });
+});
+
+menuMethods.ExampleMenu  =
+    [
+        {
+            type: "DropDown",
+            image: "cog_32x32.png",
+            text: "Navigation",
+            id: "genericDropDown",
+            submenu:
+                [
+                    {
+                        text: "Why Choose Us?",
+                        link: "http://www.google.com"
+                    },
+                    {
+                        text: "How to Sign Up!",
+                        link: "http://www.google.com"
+                    }
+                ]
+        }
+    ];
+
+menuMethods.SetCurrentMenu = function (Menu) {
+    menuMethods.CurrentMenu = Menu || {};
+}   
+
+menuMethods.LoadTopLevel = function () {
+    //$('#menuOptions').html('');
+    var Menu = menuMethods.CurrentMenu;
+    for (var i = 0; i < Menu.length; i++) {
+        var currentOption = Menu[i];
+        if(currentOption.type == "DropDown")
+        {
+            $('#mainMenu').append('<div class="NavOption NavDropDown" id="' + currentOption.id + '">' +
+                '<img src="Content\\themes\\base\\images\\' + currentOption.image + '"/>' +
+                '<h2>' + currentOption.text + '</h2>' +
+                '</div>');
+        }
+    }
+}
+
+menuMethods.ToggleSubMenu = function (targetId) {
+    var Menu = menuMethods.CurrentMenu;
+
+    if ($('#subMenu').height() < 1) {
+        
+        $('#subMenu').animate(
+            {
+                height: '72px'
+            },
+            400,
+            function () { }
+        );
+
+        var selectedItem = null;
+        for (var i = 0; i < Menu.length; i++) {
+            if (Menu[i].id == targetId) {
+                selectedItem = Menu[i];
+            }
+        }
+
+        if (selectedItem && selectedItem.submenu) {
+            for(var j = 0; j < selectedItem.submenu.length; j++)
+            {
+                var subItem = selectedItem.submenu[j];
+                $('#subMenu').append('<div class="NavOption NavLink">' +
+                    '<h2>' +
+                        subItem.text +
+                    '</h2>' +
+                '</div> ');
+            }
+        }
+
+    }
+    else {
+        $('#subMenu').animate(
+            {
+                height: '0px'
+            },
+            400,
+            function () { $('#subMenu').html('');}
+        );
+    }
+}
+
